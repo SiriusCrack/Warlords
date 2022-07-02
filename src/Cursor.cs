0975a8c80d075128d3afebaf6cc4aed6ac0feb93
MorganB <morganbrockman1998@gmail.com>
2022-07-01 18:37:06 -0700

    added variable unit types

--- src/Cursor.cs
+++ src/Cursor.cs
@@ -1,16 +1,24 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public class Cursor : Sprite
 {
     private static int[] Positions = new int[] {140, 240, 340, 440, 540, 640, 740, 840, 940};
-    private PackedScene UnitScene = ResourceLoader.Load("res://Scenes/Unit.tscn") as PackedScene;
-    private Vector2 Direction = Vector2.Right;
-    private Timer SpawnTimer;
+    [Export] private Array<PackedScene> UnitScenes;
+    private Array<Timer> SpawnTimers = new Array<Timer>();
+    [Export] private Unit.Side MySide;
+    private Vector2 Direction;
 
     private void GetInput() {
-        if (Input.IsActionJustPressed("spawn")) {
-            SpawnUnit();
+        if (Input.IsActionJustPressed("spawn1")) {
+            SpawnUnit(0);
+        }
+        if (Input.IsActionJustPressed("spawn2")) {
+            SpawnUnit(1);
+        }
+        if (Input.IsActionJustPressed("spawn3")) {
+            SpawnUnit(2);
         }
         if (Input.IsActionJustPressed("down") && Position.y < 940) {
             Position += Vector2.Down * 100;
@@ -20,38 +28,65 @@ public class Cursor : Sprite
         }
     }
 
-    private void SpawnUnit() {
-        if (SpawnTimer.IsStopped()) {
-            Area2D unit = (Area2D)UnitScene.Instance();
-            unit.Position = this.Position + (Direction*100);
+    private void SpawnUnit(int arg) {
+        GD.Print(SpawnTimers.Count);
+        if (SpawnTimers[arg].IsStopped()) {
+            Unit unit = UnitScenes[arg].Instance<Unit>();
+            unit.GetNode("SpawnTimer").QueueFree();
+            unit.MySide = MySide;
+            if (MySide == Unit.Side.Left) {
+                unit.Direction = Vector2.Right;
+            } else {
+                unit.Direction = Vector2.Left;
+                unit.Scale = new Vector2 (-1, 1);
+            }
+            unit.Position = this.Position + (Direction*75);
             Owner.AddChild(unit);
-            SpawnTimer.Start();
+            SpawnTimers[arg].Start();
+            int index = 0;
+            foreach (Timer spawnTimer in SpawnTimers) {
+                SpawnTimers[index].Start();
+                index++;
+            }
         }
     }
 
     private void AI() {
-        if (SpawnTimer.IsStopped()) {
-            Position = new Vector2 (x: Position.x, y: Positions[GD.Randi()%9]);
-            SpawnUnit();
-        }
+        // if (SpawnTimer.IsStopped()) {
+        //     Position = new Vector2 (x: Position.x, y: Positions[GD.Randi()%9]);
+        //     SpawnUnit();
+        // }
     }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GD.Randomize();
-        SpawnTimer = GetNode<Timer>("SpawnTimer");
-        if (this.Position.x < 100) {
+    public override void _Ready() {
+        if (MySide == Unit.Side.Left) {
             Direction = Vector2.Left;
+        } else {
+            Direction = Vector2.Right;
+        }
+        
+        Node spawnTimersGroup = new Node();
+        spawnTimersGroup.Name = "SpawnTimersGroup";
+        AddChild(spawnTimersGroup);
+        foreach (PackedScene unitScene in UnitScenes) {
+            Unit unit = unitScene.Instance<Unit>();
+            unit.GetNode("Sprite").QueueFree();
+            unit.GetNode("CollisionShape2D").QueueFree();
+            unit.GetNode("AttackTimer").QueueFree();
+            unit.Monitoring = false;
+            unit.Monitorable = false;
+            unit.MySide = MySide;
+            unit.Advancing = false;
+            unit.GlobalPosition = new Vector2(33,-33);
+            spawnTimersGroup.AddChild(unit);
+            SpawnTimers.Add(unit.GetNode<Timer>("SpawnTimer"));
         }
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        // if (Direction == Vector2.Left) {
+    public override void _Process(float delta) {
+        if (Direction == Vector2.Left) {
             GetInput();
-        // } else {
+        }// else {
         //     AI();
         // }
     }
--- src/Unit.cs
+++ src/Unit.cs
@@ -6,40 +6,40 @@ public class Unit : Area2D
 {
 	public enum Side {Left, Right};
 
-    [Export] private int speed;
-	private Timer AttackTimer;
-	private Array<Unit> AttackUnits = new Array<Unit>();
-	private Vector2 Direction = Vector2.Right;
-	private bool Advancing = true;
+	public Timer AttackTimer;
+	public Array<Unit> AttackUnits = new Array<Unit>();
+	public Side MySide;
+	public Vector2 Direction;
+	[Export] public bool Advancing = true;
+    [Export] public int speed;
 	[Export] public int health;
 	[Export] public int attackDamage;
-	public Side MySide = Side.Left;
 
 	public void TakeDamage(int dmg) {
 		if ((health - dmg) > 0) {
 			health -= dmg;
 		} else {
-			GetParent().RemoveChild(this);
+			QueueFree();
 		}
 	}
 
-	private void OnUnitAreaEntered(Unit area) {
-		if (area.MySide != MySide) {
+	private void OnUnitAreaEntered(Area2D area) {
+		if (((Unit)area).MySide != MySide) {
 			if (AttackUnits.Count < 1) {
 				Advancing = false;
 				AttackTimer.Start();
 			}
-			AttackUnits.Add(area);
+			AttackUnits.Add((Unit)area);
 		}
 	}
 
 	private void OnUnitAreaExited(Area2D area) {
 		if (AttackUnits.Contains((Unit)area)) {
 			AttackUnits.Remove((Unit)area);
-		}
-		if (AttackUnits.Count < 1) {
-			Advancing = true;
-			AttackTimer.Stop();
+			if (AttackUnits.Count < 1) {
+				Advancing = true;
+				AttackTimer.Stop();
+			}
 		}
 	}
 
@@ -49,24 +49,19 @@ public class Unit : Area2D
 		}
 	}
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Randomize();
-		AttackTimer = GetNode<Timer>("AttackTimer");
-		AttackTimer.WaitTime = GD.Randf() %(float)3.0;
-		health = (int)(GD.Randi() %50);
-		attackDamage = (int)(GD.Randi() %20);
-		if (this.Position.x > 100) {
-			MySide = Side.Right;
+	public override void _Ready() {
+		if (MySide == Side.Left) {
+			Direction = Vector2.Right;
+		} else {
 			Direction = Vector2.Left;
 			this.Scale = new Vector2 (-1, 1);
 		}
+        AttackTimer = GetNode<Timer>("AttackTimer");
 	}
 
 	public override void _Process(float delta) {
 		if ((Position.x < -110) || (Position.x > 2030)) {
-			GetParent().RemoveChild(this);
+			QueueFree();
 		}
 	}
 
