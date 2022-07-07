83d588c5b3d5410643a72ced327f562d8a270a7a
SiriusCrack <MorganBrockman1998@Gmail.com>
2022-07-07 15:26:18 -0700

    polished start menu & end screens

--- src/Main.cs
+++ src/Main.cs
@@ -6,8 +6,12 @@ public class Main : Node {
     [Export] PackedScene BattleScene;
     [Export] PackedScene WinScene;
     [Export] PackedScene LoseScene;
+    AudioStreamPlayer BGMusicPlayer;
+    [Export] AudioStream VictoryMusic;
+    [Export] AudioStream DefeatMusic;
 
     public override void _Ready() {
+        BGMusicPlayer = GetNode<AudioStreamPlayer>("BGMusicPlayer");
         AddChild(StartScene.Instance<Node>());
     }
 
@@ -19,10 +23,14 @@ public class Main : Node {
     public void Victory() {
         GetNode<Node>("Battle").QueueFree();
         AddChild(WinScene.Instance<Node>());
+        BGMusicPlayer.Stream = VictoryMusic;
+        BGMusicPlayer.Play();
     }
 
     public void Defeat() {
         GetNode<Node>("Battle").QueueFree();
         AddChild(LoseScene.Instance<Node>());
+        BGMusicPlayer.Stream = DefeatMusic;
+        BGMusicPlayer.Play();
     }
 }
